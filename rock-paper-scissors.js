bd4f17110f7945ad04887f8a7ddbb14e7dd05e5d
AryanAtif <aryanatif01@gmail.com>
2026-07-02 09:29:04 +0500

    feat: get a random digit from 1-3

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,18 @@
-console.log("It's working :)");
+console.log("It's working :)\n");
+let n = 25;
+let puters_choice = Math.floor(Math.random() * 4);
+while (n >= 0)
+{
+  if (puters_choice == 0)                           // since we get zero a lot less number of times than 1, 2, and 3, to get closer to 
+  {                                                // an equal probability of getting the number, we should ditch zero for an another  
+    puters_choice = Math.floor(Math.random() * 4); // number
+    continue;
+  }
+  console.log(puters_choice);
+  puters_choice = Math.floor(Math.random() * 4);
+  n--;
+}
+/*function get_computer_choice()
+{
+ eturn math  
+}*/
